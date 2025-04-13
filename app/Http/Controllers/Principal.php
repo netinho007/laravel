8ec3b1a871398e6b6ac6325b78ada92b5c02e9aa
Ícaro Redini Jaques Eid <141058011+eidicaro@users.noreply.github.com>
2025-04-13 15:45:20 -0300

    sla

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class Principal extends Controller
 {
     function principal(){
-        echo 'teste de laravel';
+        echo 'teste de laravel sla oq ta acontecendo';
     }
 
     function teste(){
